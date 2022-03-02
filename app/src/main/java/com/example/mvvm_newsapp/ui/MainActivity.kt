80a1ea9e29dba8e2f2fc010c10b1cfcf8208a43c
katarina <kat_ziv@yahoo.com>
2022-03-02 21:40:46 +0100

    init commit

---
+++ app/src/main/java/com/example/mvvm_newsapp/ui/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.example.mvvm_newsapp.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.example.mvvm_newsapp.R
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
+        val navController = findNavController(R.id.fragment)
+        val appBarConfiguration = AppBarConfiguration(setOf(
+            R.id.mainPageFragment,
+            R.id.favoritesFragment,
+            R.id.searchFragment
+        ))
+        setupActionBarWithNavController(navController,appBarConfiguration)
+        bottomNavigationView.setupWithNavController(navController)
+    }
+}
